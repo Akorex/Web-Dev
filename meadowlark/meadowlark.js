750db8f9148582fc1b62784edfabb075faf7e753
Akorede <akoredeadewole8@gmail.com>
2023-01-08 03:13:34 +0100

    form handling

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-
-
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-
 const handlers = require('./lib/handlers');
+const multiparty = require('multiparty');
 
 
 // configure Handlebars views engine
@@ -14,9 +13,31 @@ app.engine('handlebars', expressHandlebars.engine({
 }));
 
 app.set('view engine', 'handlebars');
+app.disable('x-powered-by');
 
 //static 
 app.use(express.static(__dirname + '/public'));
+
+// form handling
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newletterSignupThankYou);
+
+// handlers for JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if(err) return res.status(500).send({error: err.message})
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
 
 // routes
 app.get('/', handlers.home);
@@ -26,7 +47,6 @@ app.get('/about', handlers.about);
 app.use(handlers.notFound);
 app.use(handlers.serverError);
 
-app.disable('x-powered-by');
 
 if (require.main == module) {
     app.listen(port, () => console.log(`Express started on http://localhost:${port}
