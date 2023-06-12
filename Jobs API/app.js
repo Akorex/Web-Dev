3ea01d6d9a5e83d7564c30269e6c7013ab78fe9b
Akorede Adewole <akoredeadewole8@gmail.com>
2023-06-12 04:35:32 +0100

    added routes

--- Jobs API/app.js
+++ Jobs API/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const notFoundMiddleWare = require('./middlewares/not-found')
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+require('dotenv').config()
+require('express-async-errors')
 
 const app = express()
+app.use(express.json())
+
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+
+
 
 // set ups
-
 const port = process.env.PORT || 3000
 
 
---
+++ Jobs API/routes/jobs.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const Router = express.Router()
+
+const {getAllJobs, 
+    getJob, 
+    createJob,
+    updateJob, 
+    deleteJob
+} = require('../controllers/jobs')
+
+Router.route('/').post(createJob).get(getAllJobs)
+Router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
