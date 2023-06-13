36c5b45919f5aedb8a0e90d513e50463938a1bc7
Akorede Adewole <akoredeadewole8@gmail.com>
2023-06-13 13:38:00 +0100

    finished with register controller

--- Jobs API/controllers/auth.js
+++ Jobs API/controllers/auth.js
@@ -1,8 +1,6 @@
 const User = require('../models/user')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, UnauthenticatedError} = require('../errors')
-const bcrypt = require('bcryptjs')
-
 
 const register = async (req, res) => {
     const {name, email, password} = req.body
@@ -14,8 +12,9 @@ const register = async (req, res) => {
     }
 
     const user = await User.create({...req.body})
+    const token = user.createJWT()
 
-    res.status(StatusCodes.CREATED).json({user: {name: user.name}})
+    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
 }
 
 const login = async (req, res) => {
