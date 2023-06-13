7db97b0a01366d0015434213db351023fdf827f9
Akorede Adewole <akoredeadewole8@gmail.com>
2023-06-13 16:50:09 +0100

    fixed issues with authorization

--- Jobs API/controllers/jobs.js
+++ Jobs API/controllers/jobs.js
@@ -1,3 +1,7 @@
+const Job = require('../models/jobs')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
 const getAllJobs = async (req, res) =>{
 
 }
@@ -7,7 +11,10 @@ const getJob = async (req, res) =>{
 }
 
 const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
 
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req, res) => {
