fbad2231e47b4cec0a3cf258eb8ecac885f485c4
Akorede Adewole <akoredeadewole8@gmail.com>
2023-06-20 16:45:28 +0100

    added some packages:

--- Jobs API/app.js
+++ Jobs API/app.js
@@ -8,8 +8,27 @@ const connectDB = require('./db/connect')
 require('dotenv').config()
 require('express-async-errors')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const app = express()
 app.use(express.json())
+
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    })
+  );
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
