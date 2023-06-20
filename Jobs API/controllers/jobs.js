841b8eb8697b78a4d7eb9f8e76141b6405b147b3
Akorede Adewole <akoredeadewole8@gmail.com>
2023-06-20 15:19:05 +0100

    added create job

--- Jobs API/controllers/jobs.js
+++ Jobs API/controllers/jobs.js
@@ -8,7 +8,17 @@ const getAllJobs = async (req, res) =>{
 }
 
 const getJob = async (req, res) =>{
+    const {user: {userId}, params: {id: jobId}} = req
 
+    const job = await Job.findOne({
+        _id: jobId, createdBy: userId
+    })
+
+    if (!job){
+        throw new Error(`No job with ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req, res) => {
