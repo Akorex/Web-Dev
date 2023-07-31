96a7030e5e812df70fc138e48d4d10720e2dc294
Akorede Adewole <akoredeadewole8@gmail.com>
2023-07-31 15:55:40 +0100

    fixed conversion to pdf, working on download

--- Ilovepdf/controllers/convert.js
+++ Ilovepdf/controllers/convert.js
@@ -1,27 +1,29 @@
 // Controller for all conversion jobs
 
 const fs = require('fs')
+const path = require('path')
+const PDFDocument = require('pdfkit')
 
 const convertJpg2PDF = (req, res) => {
-    // get the file and rename to pdf
-    const file = req.file
-    //console.log(file)
-    const newFilename = (file.originalname).split('.')[0] + '.pdf'
 
-    // save to the public/files folder
-    
-    fs.rename(file.path,
-     `../public/files/${newFilename}`,
-     err => {
-        if (err){
-            console.log(err)
-        }
-        else{
-            console.log("File renamed successfully")
-        }
-     })
+    // get the file uploaded
+    const filepath = req.file.path
+    const extName = path.extname(filepath)
+    const newFilename = path.basename(filepath, extName) + '.pdf'
+    const outpath = path.join('./public/files', newFilename)
 
-    res.redirect(302,'/download')
+    const doc = new PDFDocument
+    doc.pipe(fs.createWriteStream(outpath))
+    doc.image(filepath, {
+        fit: [500, 400],
+        align: 'center',
+        valign: 'center',
+    })
+
+    doc.end()
+
+    downloadfile = res.download(outpath)
+    res.redirect('/download')
 }
 
 module.exports = {convertJpg2PDF}
