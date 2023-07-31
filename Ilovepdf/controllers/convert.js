9f7d0e1787abaa43d9ea5d47d65e7f24d325d5c2
Akorede Adewole <akoredeadewole8@gmail.com>
2023-07-31 12:39:15 +0100

    issues w/ implementing convert2pdf

--- Ilovepdf/controllers/convert.js
+++ Ilovepdf/controllers/convert.js
@@ -1,17 +1,25 @@
 // Controller for all conversion jobs
 
-const convertJpg2PDF = async (req, res) => {
+const fs = require('fs')
+
+const convertJpg2PDF = (req, res) => {
     // get the file and rename to pdf
     const file = req.file
+    //console.log(file)
     const newFilename = (file.originalname).split('.')[0] + '.pdf'
 
     // save to the public/files folder
-
-    //file.mv('./public/files' + newFilename, (err) => {
-      //  if (err){
-            //console.log(`Somthing went wrong`)
-        //}
-    //})
+    
+    fs.rename(file.path,
+     `../public/files/${newFilename}`,
+     err => {
+        if (err){
+            console.log(err)
+        }
+        else{
+            console.log("File renamed successfully")
+        }
+     })
 
     res.redirect(302,'/download')
 }
