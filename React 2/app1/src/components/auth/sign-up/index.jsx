2d2ef79837636f0cae9371fc7b1559f02897fcf6
Akorede <akoredeadewole8@gmail.com>
2024-09-04 11:36:55 +0100

    checkout

--- React 2/app1/src/components/auth/sign-up/index.jsx
+++ React 2/app1/src/components/auth/sign-up/index.jsx
@@ -1,5 +1,15 @@
+import CommonForm from "@/components/common-form"
+import { signUpFormControls } from "@/config"
+import { useForm } from "react-hook-form"
+
 function SignUp() {
-    return <div> Sign Up </div>
+    return <div> Sign Up
+
+        <CommonForm formControls={signUpFormControls} btnText={"Sign Up"} />
+
+
+
+    </div>
 }
 
 export default SignUp
---
+++ React 2/app1/src/config/index.jsx
@@ -0,0 +1,26 @@
+export const signUpFormControls = [
+    {
+        id: "name",
+        label: "Name",
+        placeHolder: "Enter your name",
+        componentType: "input",
+        type: "text"
+    },
+    
+    {
+        id: "email",
+        label: "Email",
+        placeHolder: "Enter your email",
+        componentType: "input",
+        type: "email"
+    },
+
+    {
+        id: "password",
+        label: "Password",
+        placeHolder: "Enter your password",
+        componentType: "input",
+        type: "password"
+    },
+
+]
