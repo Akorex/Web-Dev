c3ad88185cb655c94dfc15685c923535a70b49b4
Akorede <akoredeadewole8@gmail.com>
2024-09-06 15:05:25 +0100

    works

--- Carona-FE/src/pages/home.page.tsx
+++ Carona-FE/src/pages/home.page.tsx
@@ -2,8 +2,17 @@ import { CaronaCard } from "../components/home/carona-card"
 import { Navbar } from "../components/shared/navbar"
 import heroImage from "../assets/images/hero-image.png"
 import { Link } from "react-router-dom"
+import checkmark from "../assets/svg/check.svg"
+import shuttle from "../assets/images/shuttle.jpg"
 
 
+const features = [
+    "Easy-to-use web app",
+    "Professional Drivers",
+    "Clear and transparent prices",
+    "Diverse vehicles for your needs",
+  ];
+  
 
 export const HomePage = () => {
     return (
@@ -54,7 +63,64 @@ export const HomePage = () => {
                 <CaronaCard
                 title="Carona Go"
                 subtitle="Book a ride on our available routes"/>
+                
+                <CaronaCard
+                title="Carona Share"
+                subtitle="Carpool with other vetted riders" />
             </section>
+
+
+    <section className="mt-24 px-6">
+        <div className="lg:flex lg:items-center lg:justify-between max-w-[1200px] mx-auto">
+            <div className="lg:w-[48%]">
+                <h3 className="text-[#0B996F] text-4xl">
+                    Make your travel experience as easy and stress-free as possible
+                </h3>
+                <p className="mt-4">
+                At Carona, we make travel enjoyable and convenient. Our 
+                user-friendly interface allows you to quickly find and connect 
+                with carpool partners. Safety is our top priority, 
+                with thorough verification of all drivers and passengers. 
+                Enjoy a seamless, stress-free journey with us!
+                </p>
+
+            <div className="mt-4">
+              {features.map((feature, idx) => (
+                <div className="flex items-center mb-2" key={idx}>
+                  <div className="inline-block mr-2 h-[28px] w-[28px] rounded-full bg-[#E2F4EA] relative">
+                    <img
+                      src={checkmark}
+                      className="w-[10px] relative top-[50%] -translate-y-[50%] mx-auto"
+                    />
+                  </div>
+                  {feature}
+                </div>
+              ))}
+            </div>
+            </div>
+            <img className="my-8 rounded-2xl lg:w-[48%]" src={shuttle} />
+        </div>
+
+
+        <div className="md:flex md:items-center md:justify-between md:my-16 max-w-[1000px] mx-auto">
+            <div className="md:w-[48%] max-w-[480px]">
+                <h3 className="text-[#0B996F] text-4xl">
+                Ensuring your safety is guaranteed
+                </h3>
+
+                <p className="mt-4">
+                Your safety is our top priority. We ensure all drivers and 
+                passengers are thoroughly verified, and we offer in-app 
+                features like real-time tracking and emergency support. 
+                Travel with peace of mind knowing that we are committed 
+                to your safety every step of the way.
+                </p>
+
+
+            </div>
+        </div>
+
+    </section>
         </div>
     )
 }
